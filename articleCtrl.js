1a9f219a99286234ef2bbf4b4cd9ed0845e969ef
neha22prajapati <30389923+neha22prajapati@users.noreply.github.com>
2017-11-26 23:49:13 +0530

    Add files via upload

---
+++ articleCtrl.js
@@ -0,0 +1,20 @@
+﻿
+app.controller('articleCtrl', ['$scope', '$rootScope', '$state', 'articleFactory', function ($scope, $rootScope, $state, articleFactory) {
+    $scope.articleResult = articleFactory.articleReq();
+    $scope.articleData = $scope.articleResult.NewDataSet.Table1;
+    $scope.home = function(){
+        $state.go('app.home');
+    }
+
+    $scope.article = function (index) {
+        $scope.articleNav = index;
+        $state.go('app.article');
+    }
+
+}]);
+
+app.filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
---
+++ module.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+var app = angular.module('app', ['ui.router'])
+
+app.config(
+  ['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+      //$locationProvider.html5Mode(true);
+      $stateProvider
+          .state('app', {
+              //abstract: true,
+              url: '/app',
+              templateUrl: 'tpl/app.html'
+          })
+          .state('app.home', {
+              url: '/home',
+              templateUrl: 'tpl/home.html'
+          })
+          .state('app.article', {
+              url: '/article',
+              templateUrl: 'tpl/article.html'
+          })
+          .state('access', {
+              url: '/',
+              templateUrl: 'tpl/welcome.html'
+          })
+      ;
+      $urlRouterProvider.otherwise('/');
+  }]);
+  
