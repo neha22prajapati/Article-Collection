f9500fd57dc67fcec8a7c436968ebaa3a35c8378
neha22prajapati <30389923+neha22prajapati@users.noreply.github.com>
2017-11-26 23:57:57 +0530

    Add files via upload

---
+++ module.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+var app = angular.module('app', ['ui.router'])
+
+app.config(
+  ['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+      //$locationProvider.html5Mode(true);
+      $stateProvider
+          .state('app', {
+              //abstract: true,
+              url: '/app',
+              templateUrl: 'app.html'
+          })
+          .state('app.home', {
+              url: '/home',
+              templateUrl: 'home.html'
+          })
+          .state('app.article', {
+              url: '/article',
+              templateUrl: 'article.html'
+          })
+          .state('access', {
+              url: '/',
+              templateUrl: 'welcome.html'
+          })
+      ;
+      $urlRouterProvider.otherwise('/');
+  }]);
+  
